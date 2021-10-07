7dda4b08187deb1e0d174d18c94e7dc059331aea
Shikanto <antonello.anzini.comunicazioni@gmail.com>
2021-10-07 16:51:52 +0200

    finished exercise

---
+++ main.js
@@ -0,0 +1,5 @@
+let nomeUtente = prompt ("Ciao! Per creare la tua password ho bisogno di alcune informazioni, scrivi il tuo nome");
+let cognomeUtente = prompt ("Bene! Adesso ho bisogno che scrivi anche il tuo cognome");
+let colorePreferito = prompt ("Ottimo! Adesso scrivi il tuo colore preferito");
+let passwordUtente =  nomeUtente + cognomeUtente + colorePreferito;
+alert("Perfetto la tua password è: " + passwordUtente + "21");
